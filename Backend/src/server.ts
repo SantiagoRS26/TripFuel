d47bbe24fa34e63420eed5576fa597400f94bf4a
SantiagoRS <santiagors2611@gmail.com>
2025-04-19 09:12:54 -0500

    fix: 🐛 Deploy for vercel

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,12 +3,10 @@ import { createApp } from "./app";
 import { connectDB } from "./db/mongoose";
 import { config } from "./config";
 
-const start = async () => {
-	await connectDB();
-	const app = createApp();
-	app.listen(config.PORT, () => {
-		console.log(`🚀 Server running on http://localhost:${config.PORT}`);
-	});
-};
+connectDB();
+const app = createApp();
+app.listen(config.PORT, () => {
+	console.log(`🚀 Server running on http://localhost:${config.PORT}`);
+});
 
-start();
+export default app;
